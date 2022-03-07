63cab37921c2972b92631ef4075838d529dad529
Mohamed Amara <hamma.amara20@gmail.com>
2022-03-07 07:43:54 +0100

    added dashboard page changes

--- client/src/layouts/Student.js
+++ client/src/layouts/Student.js
@@ -7,11 +7,8 @@ function Student() {
   return (
     <>
       <StudentSidebar />
-      <div className="relative min-h-screen md:ml-64">
-        <DefaultHeader />
-        <div className=" min-h-full pl-4 md:pl-10 mx-auto w-full bg-white-kids  ">
-          <Outlet />
-        </div>
+      <div className="relative min-h-screen md:ml-60 pl-4 md:pl-10 bg-white-kids">
+        <Outlet />
       </div>
     </>
   );
--- client/src/views/student/dashboard/StudentDashboard.js
+++ client/src/views/student/dashboard/StudentDashboard.js
@@ -2,18 +2,16 @@ import React from "react";
 import DashboardHeader from "../../../components/Headers/DashboardHeader";
 //import TimeTable from "./CustomizedTimeline";
 import AnnouncementsList from "./AnnouncementsList";
-import CalendarBrief from "./CalendarBrief";
 import StudentInfo from "./StudentInfo";
-import VacationKnob from "./VacationKnob";
 import TodoList from "./TodoList";
-import Calendar from "./ConvertedCalendar/Calendar";
+import Calendar from "./Calendar/Calendar";
 import CustomizedTimeline from "./CustomizedTimeline";
 
 const StudentDashboard = () => {
   return (
     <div className="grid md:grid-cols-3 lg:grid-cols-5 gap-8">
       <div className="grid md:col-span-2 md:grid-cols-1 lg:col-span-4 lg:grid-cols-4 gap-4 lg:gap-8 mb-4">
-        <DashboardHeader positionInGrid="col-span-1 lg:col-span-4" />
+        <DashboardHeader  positionInGrid="col-span-1 lg:col-span-4" />
         <CustomizedTimeline positionInGrid=" lg:col-span-2 lg:row-span-2" />
         <AnnouncementsList positionInGrid="lg:col-span-2" />
         {/*   <CalendarBrief positionInGrid="xl:col-span-3" />*/}
