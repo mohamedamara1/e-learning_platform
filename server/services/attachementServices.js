eacd8ec6e8c46742d252250a8ff3f1a828cf3c93
Mohamed Amara <hamma.amara20@gmail.com>
2022-03-16 19:05:49 +0100

    Fixed attachement typos

---
+++ server/services/attachementServices.js
@@ -0,0 +1,48 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function getAttachement(Criteria) {
+  const Attachement = await prisma.attachement.findUnique({
+    where: Criteria,
+  });
+  return Attachement;
+}
+
+async function getAllAttachements() {
+  const Attachements = await prisma.attachement.findMany();
+  return Attachements;
+}
+
+async function getAttachementsByCriteria(Criteria) {
+  const Attachements = await prisma.attachement.findMany({
+    where: Criteria,
+  });
+  return Attachements;
+}
+
+async function addAttachement(Attachement) {
+  const CreateAttachement = await prisma.attachement.create({
+    data: Attachement,
+  });
+}
+
+async function updateAttachement(Criteria, AttachementData) {
+  const UpdateAttachement = prisma.attachement.update({
+    where: Criteria,
+    data: AttachementData,
+  });
+}
+
+async function deleteAttachement(Criteria) {
+  const DeleteAttachement = prisma.attachement.delete({
+    where: Criteria,
+  });
+}
+
+module.exports.getAttachement = getAttachement;
+module.exports.getAllAttachements = getAllAttachements;
+module.exports.getAttachementsByCriteria = getAttachementsByCriteria;
+module.exports.addAttachement = addAttachement;
+module.exports.updateAttachement = updateAttachement;
+module.exports.deleteAttachement = deleteAttachement;
