d4e0ee4a7b4a95d0e1558ab4e0b267bc7470c7c0
PR1DE-PY <chihaouimedmahdi@gmail.com>
2022-04-30 12:59:21 +0100

    Calendar Page

--- client/src/views/student/Calendar/StudentCalendar.js
+++ client/src/views/student/Calendar/StudentCalendar.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject } from '@syncfusion/ej2-react-schedule';
+import { Calendar, momentLocalizer} from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import 'moment-timezone'
 
-const StudentCalendar =(props) => {
-    return <ScheduleComponent currentView="Month">
-        <Inject services={[Day, Week, Month]}/>
-    </ScheduleComponent>
+const StudentCalendar = (props) => {
+    let dayStart=new Date(1972,0,1,8,0,0);
+    let dayEnd = new Date(1972,0,1,18,0,0);
+    moment.tz.setDefault('Africa/Tunis');
+    const localizer = momentLocalizer(moment);
+    return     <Calendar
+    localizer={localizer}
+    style={{padding:50, height:750}}
+    defaultView='week'
+    min={dayStart}
+    max={dayEnd}
+  />
 }
 
 export default StudentCalendar
