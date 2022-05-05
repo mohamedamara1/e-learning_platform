9272afdf126fffd3bd45e8ad5bfaf78638f2b709
Mohamed Amara <hamma.amara20@gmail.com>
2022-05-05 11:57:27 +0100

    Added change in calendar template

--- client/src/views/admin/calendar/CalendarPage.js
+++ client/src/views/admin/calendar/CalendarPage.js
@@ -2,9 +2,9 @@ import React from "react";
 import AdminHeader from "../../../components/Headers/AdminHeader";
 function CalendarPage() {
   return (
-    <div className=" flex flex-col min-h-screen bg-dark-electric-blue p-2">
+    <div className="flex flex-col min-h-screen p-2 bg-dark-electric-blue">
       <AdminHeader title="Calendar Management" background="bg-white-kids" />
-      <div className=" min-h-full  m-2 flex-grow rounded-xl p-3 bg-slate-400">
+      <div className="flex-grow min-h-full p-3 m-2 rounded-xl bg-slate-400">
         hello calendar
       </div>
     </div>
