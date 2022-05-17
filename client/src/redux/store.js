3ad9a15e42ce70d32520915c4f4c8b5011fd4f00
PR1DE-PY <chihaouimedmahdi@gmail.com>
2022-05-17 17:13:55 +0100

    redux changes

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,17 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+
 // Or from '@reduxjs/toolkit/query/react'
-import { setupListeners } from "@reduxjs/toolkit/query";
-//import { mainApi } from "../services/index";
+
 export const store = configureStore({
   reducer: {
     // Add the generated reducer as a specific top-level slice
   },
   // Adding the api middleware enables caching, invalidation, polling,
-  // and other useful features of `rtk-query`.
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware(),
+  // and other useful features of rtk-query.
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
-// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
-setupListeners(store.dispatch);
+// see setupListeners docs - takes an optional callback as the 2nd arg for customization
